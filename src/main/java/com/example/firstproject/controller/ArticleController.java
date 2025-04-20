cbd123730e1f00663bf8a2d17236cb45f0defd0d
illilili <rbgml152637@gmail.com>
2025-04-20 16:36:38 +0900

    데이터 조회

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -4,15 +4,20 @@ import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
 public class ArticleController {
 
+    private static final Logger log = LoggerFactory.getLogger(ArticleController.class);
     @Autowired // 스프링부트가 생성해둔 객체 자동 연결
     private ArticleRepository articleRepository;
 
@@ -35,6 +40,20 @@ public class ArticleController {
         log.info(saved.toString());
 
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+
+        // 1. id로 데이터를 가져옴
+        Article articleEntity =  articleRepository.findById(id).orElse(null);
+
+        // 2. 데이터를 모델에 등록
+        model.addAttribute("article", articleEntity);
+
+        // 3. 보여줄 페이지 설정
+        return "articles/show";
     }
 
 
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -6,10 +6,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity //DB가 인식
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class Article {
 
@@ -22,5 +24,7 @@ public class Article {
 
     @Column
     private String content;
+
+
 
 }
