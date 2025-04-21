c634521b237576bb5ebdc87e944a9fd16305de64
illilili <rbgml152637@gmail.com>
2025-04-21 20:43:22 +0900

    id 자동생성, 로깅 설정

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,10 +1,7 @@
 package com.example.firstproject.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +15,7 @@ import lombok.ToString;
 public class Article {
 
     @Id
-    @GeneratedValue // 자동생성
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동생성
     private Long id;
 
     @Column
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO ARTICLE(ID, TITLE, CONTENT) VALUES (1, '가가가가', '1111');
-INSERT INTO ARTICLE(ID, TITLE, CONTENT) VALUES (2, '나나나나', '2222');
-INSERT INTO ARTICLE(ID, TITLE, CONTENT) VALUES (3, '다다다다', '3333');
+INSERT INTO article(title, content) VALUES('가가가가', '1111');
+INSERT INTO article(title, content) VALUES('나나나나', '2222');
+INSERT INTO article(title, content) VALUES('다다다다', '3333');
